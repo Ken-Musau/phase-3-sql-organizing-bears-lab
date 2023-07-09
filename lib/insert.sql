610869dc6c64a8b930e04239dc1110859921e2de
Kennedy Musau <kennedy.musauu@gmail.com>
2023-07-09 11:08:41 +0300

    add sql statments

---
+++ lib/insert.sql
@@ -0,0 +1,8 @@
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Mr. Chocolate', 20, 'M', 'Black','artistic', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Rowdy', 24, 'M', 'White','Extrovert', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Tabitha', 35, 'F', 'White','Extrovert', False);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Sergeant Brown', 84, 'M', 'White','Introverted', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Melissa', 26, 'M', 'White','Extrovert', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Grinch', 24, 'F', 'White','Extrovert', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES ('Wendy', 25, 'M', 'White','Ambiert', true);
+INSERT INTO bears (name, age, sex, color, temperament, alive) VALUES (null, 24, 'M', 'White','Extrovert', true);
